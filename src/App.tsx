101afd09a43f755a98c55df9ca7134aee67623c3
Jadilson Silva <jadilsonsilva16@gmail.com>
2025-10-20 11:49:11 -0300

    Definindo Type Pessoa

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+type Pessoa = {
+  id: number;
+  nome: string;
+  idade: number;
+  email: string;
+};
+
+
 function App() {
   const [count, setCount] = useState(0)
 
